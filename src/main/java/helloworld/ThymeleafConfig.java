1383a2126300637076af60f9230bb8d7fed40446
Matthias Steiner <matthias.steiner@inscope.net>
2016-06-16 13:19:39 +0200

    switched to Java Config
    Change-Id: I5feea99b5e06a0bfdb9706136d25491f1684b68c
    Signed-off-by: Matthias Steiner <matthias.steiner@inscope.net>

---
+++ src/main/java/helloworld/ThymeleafConfig.java
@@ -0,0 +1,40 @@
+package helloworld;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration 
+@ComponentScan("helloworld") 
+public class ThymeleafConfig 
+{
+
+	@Bean 
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/WEB-INF/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("HTML5");
+		resolver.setOrder(1);
+		return resolver;
+	}
+	
+	@Bean 
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+	
+	@Bean 
+	public ThymeleafViewResolver thymeleafViewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		return resolver;
+	}
+	
+}
